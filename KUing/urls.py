9b0b33039724f9da198089bcee57d2b69b7b865c
JeonJaewon <wksk04515@naver.com>
2020-09-28 01:32:19 +0900

    DB Initialized

--- KUing/urls.py
+++ KUing/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from time_table.views import init
+from time_table.views import init, delete, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('init/', init),
-
+    path('delete/', delete),
 ]
--- time_table/models.py
+++ time_table/models.py
@@ -1,50 +1,47 @@
 from django.db import models
-import pandas as pd
+
+BUILDINGS = [
+    ('상허연구관', '상허관'),
+    ('종합강의동', '종강'),
+    ('경영관', '경영'),
+    ('산학협동관', '산학'),
+    ('신공학관', '신공'),
+    ('생명과학관', '생'),
+    ('동물생명과학관', '동'),
+    ('새천년관', '새'),
+    ('의생명과학연구관', '수'),
+    ('교육과학관', '사'),
+    ('예술문화관', '예'),
+    ('해봉부동산학관', '부'),
+    ('건축관', '건'),
+    ('인문학관', '문'),
+    ('창의관', '창'),
+    ('과학관', '이'),
+    ('공학관 A', '공A'),
+    ('공학관 B', '공B'),
+    ('공학관 C', '공C'),
+    # ('공예관', '공예'),
+]
 
 
-def init_db():
-    data = pd.read_excel("종합강의시간표내역.xlsx")[["교과목명", "강의요시/강의실", "담당교수"]]
-    print(data)
-    for i, row in data.iterrows():
+class Lecture(models.Model):
+    title = models.CharField(max_length=30)
+    professor = models.CharField(max_length=15)
+    building = models.CharField(max_length=10, default="건물이름")  # 앞차시 뒷차시 건물이 다를경우 두개의 레코드가 생성됨
 
 
 class LectureTime(models.Model):
     DAY_OF_THE_WEEK = (
-        ('MON', '월요일'),
-        ('TUE', '화요일'),
-        ('WED', '수요일'),
-        ('THR', '목요일'),
-        ('FRI', '금요일'),
-        ('SAT', '토요일'),
-        ('SUN', '일요일')
+        ('MON', '월'),
+        ('TUE', '화'),
+        ('WED', '수'),
+        ('THR', '목'),
+        ('FRI', '금'),
+        ('SAT', '토'),
+        ('SUN', '일')
     )
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    day_of_the_week = models.CharField(choices=DAY_OF_THE_WEEK, max_length=3)
-
-
-class Lecture(models.Model):
-    BUILDINGS = [
-        ('SY', '상허연구관'),
-        ('JG', '종합강의동'),
-        ('SG', '생명과학관'),
-        ('DS', '동물생명과학관'),
-        ('SH', '산학협동관'),
-        ('SC', '새천년관'),
-        ('UG', '의생명과학연구관'),
-        ('GG', '교육과학관'),
-        ('YM', '예술문화관'),
-        ('HB', '해봉부동산학관'),
-        ('GC', '건축관'),
-        ('IM', '인문학관'),
-        ('GY', '경영관'),
-        ('CU', '창의관'),
-        ('GH', '과학관'),
-        ('ENG', '공학관'),
-    ]
-
-    title = models.CharField(max_length=30)
-    building = models.CharField(choices=BUILDINGS, max_length=10)
-    time = models.ForeignKey(LectureTime, on_delete=models.CASCADE, related_name="lectures")
-    professor = models.CharField(max_length=15)
-
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, related_name="lecture_times")
+    floor = models.CharField(max_length=10)
+    start_time = models.CharField(max_length=2)
+    end_time = models.CharField(max_length=2)
+    day_of_the_week = models.CharField(max_length=1)
