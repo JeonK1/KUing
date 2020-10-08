2c5963c4510d259b7d2d260c26ecae8f10f31f9e
xemic <kan5016@naver.com>
2020-10-08 13:51:24 +0900

    room_filter.html and room_list design complete

--- time_table/urls.py
+++ time_table/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomList, index
+from .views import RoomList, index, RoomFilter
 urlpatterns = [
     path('', index),
+    path("room_list/<int:building_index>/room_filter", RoomFilter.as_view(), name="room_list"),
     path("room_list/<int:building_index>/", RoomList.as_view(), name="room_list"),
 ]
