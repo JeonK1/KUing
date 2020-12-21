3f9adde24eba2f0c539deea9600027513ce3abf7
xemic <kan5016@naver.com>
2020-12-21 10:17:31 +0900

    add desgin in mypage, reservation

--- time_table/urls.py
+++ time_table/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RoomList, index, RoomFilter, Room
+from .views import RoomList, index, RoomFilter, Room, RoomReservation, Mypage
 
 urlpatterns = [
     path('', index),
     path("room_list/<int:building_index>/room_filter", RoomFilter.as_view(), name="room_list"),
     path("room_list/<int:building_index>/", RoomList.as_view(), name="room_list"),
     path("room_list/<int:building_index>/<int:floor>", Room.as_view(), name="room_list"),
+    path("room_list/<int:building_index>/<int:floor>/reservation", RoomReservation.as_view(), name="room_list"),
+    path("mypage", Mypage.as_view(), name="mypage"),
 ]
