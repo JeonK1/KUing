2be33450d0d029e5d6fe26fd5f86b32f3cfbbd96
xemic <kan5016@naver.com>
2020-12-27 20:18:16 +0900

    fix bug

--- time_table/urls.py
+++ time_table/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomList, index, RoomFilter, Room, RoomReservation, Mypage
+from .views import RoomList, index, RoomFilter, Room, RoomReservation
 
 
 urlpatterns = [
@@ -8,5 +8,4 @@ urlpatterns = [
     path("room_list/<int:building_index>/", RoomList.as_view(), name="room_list"),
     path("room_list/<int:building_index>/<int:floor>", Room.as_view(), name="room_list"),
     path("room_list/<int:building_index>/<int:floor>/reservation", RoomReservation.as_view(), name="room_list"),
-    path("mypage", Mypage.as_view(), name="mypage"),
 ]
